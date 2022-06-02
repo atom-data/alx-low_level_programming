1a051716faf15534bb2d9894782eaa0f6f2d8c12
atom-data <alatatomondi@gmail.com>
2022-06-02 15:10:12 -0700

    print a statement on the standard output

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a text and add a new line
+ *
+ * Return: 0 - after succesful compilation and execution
+ *
+ */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
