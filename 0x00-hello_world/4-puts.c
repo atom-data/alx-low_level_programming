863f71f7c37ee58bb890fc8fb1f5498a7a9058ad
atom-data <alatatomondi@gmail.com>
2022-06-02 14:56:05 -0700

    display a text using put on the standard output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - output using the put function
+ *
+ * Description: print text on the standard output
+ * Return: 0 - when succesfully compiled
+ */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
+
