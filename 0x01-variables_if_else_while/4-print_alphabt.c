405d172102320da9ec8313cb43daf7de1c81deac
atom-data <alatatomondi@gmail.com>
2022-06-03 17:48:22 -0700

    print lowercase alphabet except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Print lowercase alphabet followed by a line
+ *
+ * Return: compiles successfully
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' || c != 'q')
+		{
+			putchar(c);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
