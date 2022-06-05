6ed911def5bec26d6925c7ceecc4ed46951520ec
atom-data <alatatomondi@gmail.com>
2022-06-05 01:02:32 -0700

    Print all combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Print all possible combinations of two digits
+ *
+ * Return: successful compilation
+ */
+int main(void)
+{
+	int i;
+	int k;
+
+	for (i = '0'; i <= '8'; i++)
+	{
+		for (k = i + 1; k <= '9'; k++)
+		{
+			putchar(i);
+			putchar(k);
+
+   			if (i != '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
