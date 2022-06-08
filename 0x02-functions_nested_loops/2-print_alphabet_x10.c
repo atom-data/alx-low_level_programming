3c10ed26472ac3abc9a1be083c263480b646c0b0
atom-data <alatatomondi@gmail.com>
2022-06-08 07:44:02 -0700

    Print lowercase alphabet in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Do nothing
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+}
