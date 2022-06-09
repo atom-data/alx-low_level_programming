b6823d862f7909530e986a1c8bfe0a937244dd6d
atom-data <alatatomondi@gmail.com>
2022-06-09 00:59:46 -0700

    FizzBuzz coding test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Print 1 to 100, with Fizz, Buzz and FizzBuzz at certain locations
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		int j;
+		int k;
+		int l;
+
+		j = i % 3;
+		k = i % 5;
+		l = j + k;
+
+		if (l == 0)
+			printf("%s ", "FizzBuzz");
+		else if (k == 0)
+			printf("%s ", "Buzz");
+		else if (j == 0)
+			printf("%s ", "Fizz");
+		else
+			printf("%d ", i);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
