be049412bc46458cf560f83561314711e1a6f6ac
atom-data <alatatomondi@gmail.com>
2022-06-09 10:03:12 -0700

    Print digits from 0 to 9 on the terminal

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	char c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
