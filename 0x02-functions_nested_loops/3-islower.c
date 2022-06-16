c6ca8ef7bf9d82db13f292a130c9e264906a9544
atom-data <alatatomondi@gmail.com>
2022-06-16 12:54:09 -0700

    update islower script

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- *_islower - check for lowercase
-*@c: character to be checked
-*
-* Return: 1 if lowercase or 0 otherwise
-*/
+ * _islower - checks for lowercase
+ * @c: integer to be checked for lowercase
+ *
+ * Return: 1 if lowercase, 0 otherwise
+ */
 
 int _islower(int c)
 {
-	if ((c >= 97) && (c =< 122))
+	if ((c >= 97) && (c <= 122))
 	{
 		return (1);
 	}
-
 	return (0);
 }
