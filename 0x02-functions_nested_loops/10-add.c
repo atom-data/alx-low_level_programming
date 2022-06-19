139d3d21e1f3215145ee00a79e7248d63fd272a0
atom-data <alatatomondi@gmail.com>
2022-06-19 07:18:55 -0700

    sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - sum to integers
+ * @i: first input
+ * @j: second input
+ * Return: the sum of the two inputs
+ */
+
+int add(int i, int j)
+{
+	int k;
+
+	k = i + j;
+
+	return (k);
+}
