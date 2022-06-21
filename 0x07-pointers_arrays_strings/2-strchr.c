8dd6e4abe78dc7596eb76a8ac14f75024b327954
atom-data <alatatomondi@gmail.com>
2022-06-21 10:13:37 -0700

    locate address of character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locate first occurrence of a character in a string
+ * @s: string to be checked
+ * @c: character to be checked
+ *
+ * Return: Address of the character
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *a;
+
+	for (a = s; *a != '\0'; a++)
+	{
+		if (*a == c)
+		{
+			return (a);
+		}
+	}
+	return (0);
+}
