7cb5dd1ba0bc68ab7043f10dbc2654525fde81a8
atom-data <alatatomondi@gmail.com>
2022-06-23 02:52:10 -0700

    print a string of characters followed by a newline

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -10,8 +10,8 @@ int main(void)
 {
 	char *str;
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov"
-		_puts(str);
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
 
 	return (0);
 }
