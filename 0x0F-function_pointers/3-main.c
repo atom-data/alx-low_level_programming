3b1945de4c8e9ffdf589f011ab669f24e7601997
atom-data <alatatomondi@gmail.com>
2022-07-06 16:42:00 -0700

    final file for num 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - perform simple operations
+ * @argc: total number of arguments
+ * @argv: array of strings
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, c;
+
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	f = get_op_func(argv[2]);
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	c = f(a, b);
+	printf("%d\n", c);
+	return (0);
+}
