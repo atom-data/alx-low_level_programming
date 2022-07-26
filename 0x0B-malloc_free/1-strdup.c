d802347d5bcaa89cf616d269df1a5babbad4ba33
atom-data <alatatomondi@gmail.com>
2022-07-26 06:13:47 -0700

    string duploication

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strdup - create a copy of a string
+ * @str: string to be copied
+ *
+ * Return: pointer to a newly allocated memory
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i;
+	unsigned int k;
+	unsigned int sum = 0;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		sum += 1;
+	}
+	ptr = malloc(sizeof(char) * sum);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (k = 0; *(str + k) != '\0'; k++)
+	{
+		ptr[k] = *(str + k);
+	}
+	return (ptr);
+}
