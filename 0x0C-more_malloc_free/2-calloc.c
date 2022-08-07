b46aabfdd41214a95defb5d1d9bac94bd287432c
atom-data <alatatomondi@gmail.com>
2022-08-07 02:37:41 -0700

    allocate memory for array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _calloc - allocate memory for an array
+ * @nmemb: number of element in the array
+ * @size: size of each element of an array
+ *
+ * Return: pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i;
+
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < nmemb * size)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	return (ptr);
+
+}
