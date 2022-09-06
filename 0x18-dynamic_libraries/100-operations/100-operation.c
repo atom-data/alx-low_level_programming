95e8b8f249c5ca7c8cd149d3b5ac51b320cf9acf
atom-data <alatatomondi@gmail.com>
2022-09-05 17:14:10 -0700

    dynamic C library called in python

---
+++ 0x18-dynamic_libraries/100-operations/100-operation.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/**
+ * add - sum of two integers
+ * @a: first input
+ * @b: second input
+ *
+ * Return: Always an integer
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - difference between two integers
+ * @a: first input
+ * @b: second input
+ *
+ * Return: Always an integer
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - product of two integers
+ * @a: first input
+ * @b: second input
+ *
+ * Return: Always an integer
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * mod - remainder
+ * @a: number
+ * @b: divisor
+ *
+ * Return: Always an integer
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * div - quotient
+ * @a: number
+ * @b: divisor
+ *
+ * Return: Always an integer
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
