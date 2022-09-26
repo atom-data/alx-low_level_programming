984403dabf169271c37b596d990979b51afd345b
atom-data <alatatomondi@gmail.com>
2022-09-26 04:25:57 -0700

    delete a hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - delete hash table
+ * @ht: hash table to be deleted
+ *
+ * Return: Nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *tmp;
+
+	if (ht == NULL || ht->size == 0 || ht->array == NULL)
+		return;
+	i = 0;
+	while(i < ht->size)
+	{
+		while (ht->array[i] != NULL)
+		{
+			tmp = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = tmp;
+		}
+		i++;
+	}
+	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
+	free(ht);
+}
