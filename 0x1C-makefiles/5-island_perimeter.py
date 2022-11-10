72f1e9391b91ba809374c975aa57dc79622d8b63
atom-data <alatatomondi@gmail.com>
2022-11-10 04:08:43 -0800

    island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,16 +4,17 @@
 
 def island_perimeter(grid):
     """Calculate island perimeter"""
-    perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                if i == 0 or grid[i - 1][j] == 0:
-                    perimeter += 1
-                if i == len(grid) or grid[i + 1][j] == 0:
-                    perimeter += 1
-                if j == len(grid[i]) or grid[i][j + 1] == 0:
-                    perimeter += 1
-                if j == 0 or grid[i][j - 1] == 0:
-                    perimeter += 1
+    if grid != []:
+        perimeter = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == 1:
+                    if i == 0 or grid[i - 1][j] == 0:
+                        perimeter += 1
+                    if i == len(grid) or grid[i + 1][j] == 0:
+                        perimeter += 1
+                    if j == len(grid[i]) or grid[i][j + 1] == 0:
+                        perimeter += 1
+                    if j == 0 or grid[i][j - 1] == 0:
+                        perimeter += 1
     return perimeter
