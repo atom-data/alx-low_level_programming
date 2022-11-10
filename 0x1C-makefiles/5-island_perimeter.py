53eb9197410d4dc996e81705f8e5b8faad39d10c
atom-data <alatatomondi@gmail.com>
2022-11-10 08:35:20 -0800

    corrected code

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,9 +11,9 @@ def island_perimeter(grid):
                 if grid[i][j] == 1:
                     if i == 0 or grid[i - 1][j] == 0:
                         perimeter += 1
-                    if i == len(grid) or grid[i + 1][j] == 0:
+                    if i == (len(grid) - 1) or grid[i + 1][j] == 0:
                         perimeter += 1
-                    if j == len(grid[i]) or grid[i][j + 1] == 0:
+                    if j == (len(grid[i]) - 1) or grid[i][j + 1] == 0:
                         perimeter += 1
                     if j == 0 or grid[i][j - 1] == 0:
                         perimeter += 1
